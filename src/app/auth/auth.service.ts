257035b23503429220c9dee771eb654140978300
Mastro <Mastrangelo470@gmail.com>
2024-04-16 11:25:49 +0200

    deploy

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { catchError, tap } from "rxjs/operators";
 import { BehaviorSubject, Subject, throwError } from "rxjs";
 import { User } from "./user.model";
 import { Router } from "@angular/router";
+import { environment } from "../../environments/environment";
 
 
 export interface AuthResponseData {
@@ -27,7 +28,7 @@ export class AuthService {
 
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>( //l'api dice che restituisce l'oggetto definito nell'interfaccia sopra, quindi con gli angolari possiamo indicare che tipo di oggetto verrà restituito
-            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBtF8Kn6F_yuh8QFXH_L8wIQRb0NngAm1s', // La prima parte di link è dove mandare la richiesta(in questo caso all'api che permette di fare il sing in fornita da firebase, la parte dopo "=" è la chiave del nostro db che si prendere dalle impostazioni)
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey, // La prima parte di link è dove mandare la richiesta(in questo caso all'api che permette di fare il sing in fornita da firebase, la parte dopo "=" è la chiave del nostro db che si prendere dalle impostazioni)
         {   //guardando l'api dice che ha bisogno di email password ed un token sempre vero, quindi gli passiamo quelli
             email: email,
             password: password,
@@ -49,7 +50,7 @@ export class AuthService {
 
     login(email: string, password: string) {
         return this.http.post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBtF8Kn6F_yuh8QFXH_L8wIQRb0NngAm1s',
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
         {
             email: email,
             password: password,
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    firebaseAPIKey: 'AIzaSyBtF8Kn6F_yuh8QFXH_L8wIQRb0NngAm1s'
+};
